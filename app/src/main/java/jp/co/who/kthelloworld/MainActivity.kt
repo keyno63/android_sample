d4c8b9989e9c20cb86c41bf159e55420d5f2e0ad
Keyno <kofujiw@gmail.com>
2019-09-08 21:18:32 +0900

    画面遷移機能実装.

--- app/src/main/java/jp/co/who/kthelloworld/MainActivity.kt
+++ app/src/main/java/jp/co/who/kthelloworld/MainActivity.kt
@@ -1,12 +1,26 @@
 package jp.co.who.kthelloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
+    val EXTRA_MESSAGE: String = "jp.co.who.kthelloworld"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Send ボタンクリック時
+    fun sendMessage(view: View) {
+        val intent: Intent = Intent(this@MainActivity,
+            DisplayMessageActivity::class.java)
+        val editText: EditText = findViewById(R.id.editText) as EditText
+        val message: String = editText.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
+        startActivity(intent)
     }
 }
